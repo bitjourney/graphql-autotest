97b4d46f82f0c2f450d9e7018a66ac9f88540d65
Ioannis Kolovos <yannis.kolovos@gmail.com>
2020-03-26 00:53:27 +0200

    add debug output

--- lib/graphql/autotest/runner.rb
+++ lib/graphql/autotest/runner.rb
@@ -1,20 +1,21 @@
 module GraphQL
   module Autotest
     class Runner
-      attr_reader :schema, :context, :arguments_fetcher, :max_depth, :skip_if
-      private :schema, :context, :arguments_fetcher, :max_depth, :skip_if
+      attr_reader :schema, :context, :arguments_fetcher, :max_depth, :skip_if, :debug
+      private :schema, :context, :arguments_fetcher, :max_depth, :skip_if, :debug
 
       # @param schema [Class<GraphQL::Schema>]
       # @param context [Hash] it passes to GraphQL::Schema.execute
       # @param arguments_fetcher [Proc] A proc receives a field and ancestors keyword argument, and it returns a Hash. The hash is passed to call the field.
       # @param max_depth [Integer] Max query depth. It is recommended to specify to avoid too large query.
       # @param skip_if [Proc] A proc receives a field and ancestors keyword argument, and it returns a boolean. If it returns ture, the field is skipped.
-      def initialize(schema:, context:, arguments_fetcher: ArgumentsFetcher::DEFAULT, max_depth: 10, skip_if: -> (_field, **) { false })
+      def initialize(schema:, context:, arguments_fetcher: ArgumentsFetcher::DEFAULT, max_depth: 10, skip_if: -> (_field, **) { false }, debug:)
         @schema = schema
         @context = context
         @arguments_fetcher = arguments_fetcher
         @max_depth = max_depth
         @skip_if = skip_if
+        @debug = debug
       end
 
       def report(dry_run: false)
@@ -28,7 +29,9 @@ module GraphQL
         )
         fields.each do |f|
           q = f.to_query
-
+          
+          puts "Running Query: #{f.name}" if debug
+          
           result = if dry_run
                      {}
                    else
